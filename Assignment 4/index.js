53d3c332aacd562aaf350b28b6a3c94cc78bc093
Yixin Cao <54694766+SharonCao0920@users.noreply.github.com>
2023-09-25 21:57:26 -0700

    getUser

--- Assignment 4/index.js
+++ Assignment 4/index.js
@@ -3,30 +3,25 @@ const fs = require('fs');
 const express = require('express');
 const startUp = require('./routes/startup');
 const getDevice = require('./routes/getDevice');
+const getUser = require('./routes/getUser');
 
 const port = 8080
+// Create https options
 const httpsOptions = {
     key: fs.readFileSync('./ssl//key.pem'),
     cert: fs.readFileSync('./ssl/cert.pem')
 }
 
+// Create an express app
 app = express();
 
+// Create a https server
 const server = https.createServer(httpsOptions,app);
 
 app.use(express.json());
 app.use('/', startUp)
 app.use('/', getDevice)
-
-// app.get('/get-device', (req, res) => {
-//   const whatDeviceIsThis = req.header('User-Agent');
-//   //If the device is windows, send a flg: isWindows=true
-//   //If the device is mac, send a flg: isMac=true
-//   //If the device is linux, send a flg: isLinux=true
-  
-  
-//   res.send({whatDeviceIsThis});
-// });
+app.use('/', getUser)
 
 app.get('/get-ip', (req, res) => {
   const userIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
